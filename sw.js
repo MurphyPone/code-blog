fef2a3302265fd26eed65a8ade1ab20cfc5afd77
MurphyPOne <MurphyP1@bishopireton.org>
2019-04-22 20:28:08 -0400

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-eeb709b39a6dae2bf7ae.js"
+    "url": "webpack-runtime-4868f96029afd051e679.js"
   },
   {
     "url": "app-ac1c828a0592c354591e.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "72094294cd76c9da77705205d52a2f4f"
+    "revision": "dbe52874a2c8b0e8bbdefa1e985a4f07"
   },
   {
     "url": "0-4b350334d6eb4a2a1180.js"
